e2ce6467ef6ed5393d36275e6e0de480a87226ff
Stella <stellaliuca@gmail.com>
2023-03-02 08:43:53 -0800

    styling name

--- application/client/src/pages/SamuelElias.js
+++ application/client/src/pages/SamuelElias.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 function SamuelElias() {
   return (
     <div>
-      <Link to="/">
-        Home page
-      </Link>
-      <h2> Samuel Elias</h2>
+      <Link to="/">Home page</Link>
+      <h2 className="names"> Samuel Elias</h2>
+      <h4 className="introduce">{/* write here */}</h4>
     </div>
-  )
+  );
 }
 
-export default SamuelElias
+export default SamuelElias;
