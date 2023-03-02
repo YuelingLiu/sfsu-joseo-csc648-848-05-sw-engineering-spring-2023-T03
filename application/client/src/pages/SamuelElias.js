0a3fa3ad5d4c23e7c72fd4eecc679e7edd8dc88b
Nathan <nateyl.b@gmail.com>
2023-03-02 15:56:17 -0800

    fixed sam page

--- application/client/src/pages/SamuelElias.js
+++ application/client/src/pages/SamuelElias.js
@@ -1,20 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 function SamuelElias() {
   return (
     <div>
-      <Link to="/">
-        Home page
-      </Link>
+      <Link to="/">Home page</Link>
+
       <h2 className="names">Samuel Elias</h2>
-      <h4 className="introduce"> 
+      <h4 className="introduce">
       Welcome to the about me page for our webapp development project RecipeReel. My name is Samuel Elias I am a senior computer science student with a passion for software engineering. 
       Throughout my academic journey, I have developed a wide range of skills in both front-end and back-end development, with as well as on database management. As I am very determined  to be the database master for this project. 
       I also look forward to collaborate with a team of talented individuals to create a very incredible and entertaining web application that will make a positive impact in the lives of its users. I am committed to ensuring that the database component of this application is reliable and user-friendly.
-
       </h4>
-      </div>
-  )
+    </div>
+  );
 }
 
-export default SamuelElias
+export default SamuelElias;
