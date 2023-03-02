1567df21b18bfd7007b8d501d7259677bc056e31
Samuel Elias <44490521+samfikre13@users.noreply.github.com>
2023-03-02 15:19:00 -0800

    Update SamuelElias.js

--- application/client/src/pages/SamuelElias.js
+++ application/client/src/pages/SamuelElias.js
@@ -7,7 +7,13 @@ function SamuelElias() {
         Home page
       </Link>
       <h2> Samuel Elias</h2>
-    </div>
+      <h4 className="introduce"> 
+      Welcome to the about me page for our webapp development project RecipeReel. My name is Samuel Elias I am a senior computer science student with a passion for software engineering. 
+      Throughout my academic journey, I have developed a wide range of skills in both front-end and back-end development, with as well as on database management. As I am very determined  to be the database master for this project. 
+      I also look forward to collaborate with a team of talented individuals to create a very incredible and entertaining web application that will make a positive impact in the lives of its users. I am committed to ensuring that the database component of this application is reliable and user-friendly.
+
+      </h4>
+      </div>
   )
 }
 
