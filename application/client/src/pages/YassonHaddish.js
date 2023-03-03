62cb179f424bdbdab076267a20a2afee533ea46c
Yassonh <yassonhaileselassie@gmail.com>
2023-03-02 18:35:55 -0800

    commit Yasson

--- application/client/src/pages/YassonHaddish.js
+++ application/client/src/pages/YassonHaddish.js
@@ -9,7 +9,7 @@ function YassonHaddish() {
       <h4 className="introduce">
         Hello, I am SFSU CS student on my final year. I document for the
         website RecipeReel that is aiming to revolutionize the food search industry.
-        I love playing basketball and go for a walk. I also like to play video games 
+        I love playing basketball and go for a long walk. I also like to play video games 
         and make small demos on my free time. I enjoy eating good food and experience 
         new meal from different places, that is why I am passionate about this project. 
       </h4>
