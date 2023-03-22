85554ecdaa686e492bf2fdb1497a7a0c93d832e5
Nathan <nateyl.b@gmail.com>
2023-03-21 18:55:36 -0700

    added PG server

--- application/server/server.js
+++ application/server/server.js
@@ -1,14 +1,31 @@
-const http = require('http');
+require("dotenv").config();
+const express = require("express");
+const { Client } = require("pg");
 
-// const hostname = 'localhost';
-const port = 3000;
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+const PORT = process.env.PORT || 3000;
 
-server.listen(port, () => {
-  console.log(`Server running at posr: ${port}`);
-});
+const connectDb = async () => {
+  try {
+    const client = new Client({
+      user: process.env.PGUSER,
+      host: process.env.PGHOST,
+      database: process.env.PGDATABASE,
+      password: process.env.PGPASSWORD,
+      port: process.env.PGPORT,
+    });
+    console.log(client);
+    await client.connect();
+    const res = await client.query("SELECT * FROM some_table");
+    console.log(res);
+    await client.end();
+  } catch (error) {
+    console.log(error);
+  }
+};
+connectDb();
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+app.listen(PORT, () => console.log(`Server listening in port ${PORT}`));
