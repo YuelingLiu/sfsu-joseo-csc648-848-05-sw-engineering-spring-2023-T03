ea2364cd3b4e505ce6b63f1bc56826e1c81b5db0
Nathan <nateyl.b@gmail.com>
2023-04-04 14:13:48 -0700

    removed port # bug

--- application/client/src/pages/Search.js
+++ application/client/src/pages/Search.js
@@ -9,7 +9,7 @@ const Search = ({ location }) => {
         const query = new URLSearchParams(location.search).get('query');
         try {
           console.log("query in search: " + query);
-          const response = await fetch(`https://recipereel.me:3000/search?query=${query}`);
+          const response = await fetch(`https://recipereel.me:search?query=${query}`);
           const data = await response.json();
   
           if (response.ok) {
