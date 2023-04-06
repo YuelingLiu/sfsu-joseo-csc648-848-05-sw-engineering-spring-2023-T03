41ca420ab3624604718f84cbb23aaf7be0cfc7f2
Stella <stellaliuca@gmail.com>
2023-04-06 13:15:23 -0700

    fixing register page

--- application/client/src/pages/Register.js
+++ application/client/src/pages/Register.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Styling/register.scss';
 
@@ -8,18 +8,19 @@ const Register = () => {
       <div className="card">
         <div className="left">
           <h2> Welcome to RecipeReel</h2>
-          <p>Do you have an account?</p>
+          <p>You already have an account?</p>
           <Link to="/login">
             <button>Login</button>
           </Link>
         </div>
+
         <div className="right">
           <h1>Register</h1>
           <form>
             <input type="text" placeholder="Username" />
             <input type="password" placeholder="Password" />
-            <input type="email" placeholder="email" />
-            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Confirm password" />
+            <input type="email" placeholder="Email@address.com" />
 
             <button>Register</button>
           </form>
