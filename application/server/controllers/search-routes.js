c0e84f6c4af316d0d18b7ea31f406ea3cb96125b
Duncan Herington <87780351+Dherington1@users.noreply.github.com>
2023-04-05 17:26:32 -0700

    query that is working in pgAdmin4 has been added. **TO DO** fix const results in search-routes.js to actually go through the db

--- application/server/controllers/search-routes.js
+++ application/server/controllers/search-routes.js
@@ -1,33 +1,33 @@
 const router = require("express").Router();
-const Recipe = require('../models/Recipe');
 
 // search 
 router.get('/', async (req, res) => {
   try {
-    const searchTerm = req.query.query; // Assuming the search term is passed as a query parameter
-
+    const searchTerm = req.query.query; 
     const searchQuery = `
-    WITH search_result AS (
-      SELECT DISTINCT r."ID" AS recipe_id
-      FROM public.recipes r
-      JOIN public.users u ON r."userID" = u."ID"
-      JOIN public.categories_to_recipe ctr ON r."ID" = ctr."ID"
-      JOIN public.categories c ON ctr."ID" = c."ID"
-      JOIN public.comments cm ON r."ID" = cm."ID"
-      JOIN public.ingredients i ON r."ID" = i."ID"
-      JOIN public.instructions ins ON r."ID" = ins."ID"
-      JOIN public.ratings rt ON r."ID" = rt."ID"
-      WHERE (r.title ILIKE '%biscuits%' 
-          OR r.description ILIKE '%biscuits%' 
-          OR c.category ILIKE '%biscuits%' 
-          OR u.username ILIKE '%biscuits%' 
-          OR cm.comment ILIKE '%biscuits%' 
-          OR i.ingredient ILIKE '%biscuits%' 
-          OR ins.instruction ILIKE '%biscuits%')
-  )
-  SELECT * FROM search_result
-  ORDER BY recipe_id ASC;
-    `;
+      WITH search_result AS (
+        SELECT DISTINCT r."ID" AS recipe_id, r."title" AS recipe_title
+        FROM public.recipes r
+        JOIN public.users u ON r."userID" = u."ID"
+        LEFT JOIN public.categories_to_recipe ctr ON r."ID" = ctr."recipeID"
+        LEFT JOIN public.categories c ON ctr."categoryID" = c."ID"
+        LEFT JOIN public.comments cm ON r."ID" = cm."recipeID"
+        LEFT JOIN public.ingredients i ON r."ID" = i."recipeID"
+        LEFT JOIN public.instructions ins ON r."ID" = ins."recipeID"
+        LEFT JOIN public.ratings rt ON r."ID" = rt."recipeID"
+        WHERE (
+            r.title ILIKE '%${searchTerm}%' 
+            OR r.description ILIKE '%${searchTerm}%' 
+            OR c.category ILIKE '%${searchTerm}%' 
+            OR u.username ILIKE '%${searchTerm}%' 
+            OR cm.comment ILIKE '%${searchTerm}%' 
+            OR i.ingredient ILIKE '%${searchTerm}%' 
+            OR ins.instruction ILIKE '%${searchTerm}%'
+        )
+      )
+      SELECT * FROM search_result
+      ORDER BY recipe_id ASC;
+  `;
 
     const results = await db.any(searchQuery, [`%${searchTerm}%`]);
     res.json(results);
