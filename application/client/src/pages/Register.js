7c0f511ecaae5a20dedb754d82528062ee10fe22
Stella <stellaliuca@gmail.com>
2023-04-07 23:01:34 -0700

    added hooks

--- application/client/src/pages/Register.js
+++ application/client/src/pages/Register.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Styling/register.scss';
-
+import { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
 const Register = () => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  // States for checking the errors
+  const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState(false);
+  // const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    //testing
+    console.log(userName, password, confirmPassword, email);
+    e.preventDefault();
+    if (
+      userName === '' ||
+      password === '' ||
+      confirmPassword === '' ||
+      email === ''
+    ) {
+      setError(true);
+    } else {
+      setSubmitted(true);
+      setError(false);
+      <Link to="/profile" />;
+    }
+  };
+
   return (
     <div className="register">
       <div className="card">
@@ -17,12 +45,37 @@ const Register = () => {
         <div className="right">
           <h1>Register</h1>
           <form>
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-            <input type="password" placeholder="Confirm password" />
-            <input type="email" placeholder="Email@address.com" />
-
-            <button>Register</button>
+            <input
+              type="text"
+              placeholder="Username"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              id="userName"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              id="password"
+            />
+            <input
+              type="password"
+              placeholder="Confirm password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              id="confirmPassword"
+            />
+            <input
+              type="email"
+              placeholder="Email@address.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              id="email"
+            />
+            <button className="submitBtn" onClick={handleSubmit}>
+              Register
+            </button>
           </form>
         </div>
       </div>
