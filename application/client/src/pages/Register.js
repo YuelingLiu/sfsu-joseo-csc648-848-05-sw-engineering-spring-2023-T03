3c8f57610a6569307ee0ee1ffde6a67902a2d54c
Stella <stellaliuca@gmail.com>
2023-04-08 22:48:53 -0700

    added client-side validation

--- application/client/src/pages/Register.js
+++ application/client/src/pages/Register.js
@@ -2,33 +2,73 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Styling/register.scss';
 import { useState } from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 // import { useNavigate } from 'react-router-dom';
 const Register = () => {
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [validationErrors, setValidationErrors] = useState({});
+  toast.configure();
 
-  // States for checking the errors
-  const [submitted, setSubmitted] = useState(false);
-  const [error, setError] = useState(false);
-  // const navigate = useNavigate();
   const handleSubmit = (e) => {
-    //testing
-    console.log(userName, password, confirmPassword, email);
     e.preventDefault();
-    if (
-      userName === '' ||
-      password === '' ||
-      confirmPassword === '' ||
-      email === ''
-    ) {
-      setError(true);
-    } else {
-      setSubmitted(true);
-      setError(false);
-      <Link to="/profile" />;
+    const errors = {};
+
+    if (userName.length < 6 || userName.length > 20) {
+      errors.username = 'Username must be between 6 and 20 characters';
+
+      toast.error('Username must be between 6 and 20 characters', {
+        position: toast.POSITION.TOP_CENTER,
+        className: 'toast-message',
+      });
+      console.log('Invalid Username');
     }
+
+    if (
+      !password.match(
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+~`|}{\[\]\\:;'<>,.?/])[a-zA-Z\d!@#$%^&*()_+~`|}{\[\]\\:;'<>,.?/]{8,20}$/
+      )
+    ) {
+      errors.password =
+        'Password must be a combination letters, numbers, and special  with a maximum of 20 length';
+      toast.error(errors.password, {
+        position: toast.POSITION.TOP_CENTER,
+        className: 'toast-message',
+      });
+      console.log('Invalid password');
+    }
+
+    if (password !== confirmPassword) {
+      errors.confirmPassword = 'Passwords do not match';
+      toast.error(errors.confirmPassword, {
+        position: toast.POSITION.TOP_CENTER,
+        className: 'toast-message',
+      });
+    }
+
+    if (
+      !email.match(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)
+    ) {
+      errors.email = 'Please enter a valid email address';
+      toast.error(errors.email, {
+        position: toast.POSITION.TOP_CENTER,
+        className: 'toast-message',
+      });
+    }
+
+    if (Object.keys(errors).length > 0) {
+      setValidationErrors(errors);
+      toast.error('Invalid Input', {
+        position: toast.POSITION.TOP_CENTER,
+        className: 'toast-message',
+      });
+    }
+
+    // submit form
   };
 
   return (
@@ -44,10 +84,11 @@ const Register = () => {
 
         <div className="right">
           <h1>Register</h1>
-          <form>
+          <form onSubmit={handleSubmit}>
             <input
               type="text"
               placeholder="Username"
+              required={true}
               value={userName}
               onChange={(e) => setUserName(e.target.value)}
               id="userName"
