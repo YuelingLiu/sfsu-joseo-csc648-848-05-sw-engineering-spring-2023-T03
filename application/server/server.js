5ea52a43407f71c2d05f283655a4980e3b7efafd
Nathan <nateyl.b@gmail.com>
2023-04-11 16:32:57 -0700

    added app.use(cors)

--- application/server/server.js
+++ application/server/server.js
@@ -12,6 +12,7 @@ const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use("/search", searchRoutes);
 app.get("/", (req, res) => res.send("Hello World"));
 
