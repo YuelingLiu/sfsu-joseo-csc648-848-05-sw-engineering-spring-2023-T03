2c88ac9e9598185e084a20b11f0be745c906c7f4
Nathan <nateyl.b@gmail.com>
2023-04-12 22:34:02 -0700

    added uuid

--- application/server/controllers/user-routes.js
+++ application/server/controllers/user-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
 const { client } = require("../db/db"); 
 const User = require("../models/User");
 
