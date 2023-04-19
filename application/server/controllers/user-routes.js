fd9afcbbafff8c999a35787e9b0a0fae1b71b785
Stella <stellaliuca@gmail.com>
2023-04-18 20:02:51 -0700

    pulled code

--- application/server/controllers/user-routes.js
+++ application/server/controllers/user-routes.js
@@ -67,8 +67,8 @@ router.post('/register', upload.single('profile_picture'), async (req, res) => {
   try {
     console.log('in register route');
     // take input from website
-    const username = req.body.username
-    console.log("this is username:" + username);
+    const username = req.body.username;
+    console.log('this is username:' + username);
     const email = req.body.email;
     const password = req.body.password;
 
@@ -87,16 +87,16 @@ router.post('/register', upload.single('profile_picture'), async (req, res) => {
     const file = req.file;
     var newUser;
     const uploadImg = (file) => {
-      // check if buffer is working 
+      // check if buffer is working
       if (!file.buffer) {
         return Promise.reject(new Error('File buffer is not available'));
       }
-      const uploadParams = { 
-        Bucket: process.env.AWS_BUCKET_NAME, 
-        Key: uuidv4() + '-' + file.originalname, 
-        Body: file.buffer, 
-        ContentEncoding: 'base64', 
-        ContentType: file.mimetype, 
+      const uploadParams = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: uuidv4() + '-' + file.originalname,
+        Body: file.buffer,
+        ContentEncoding: 'base64',
+        ContentType: file.mimetype,
       };
       return new Promise(async (resolve, reject) => {
         await s3.upload(uploadParams, async function (err, data) {
