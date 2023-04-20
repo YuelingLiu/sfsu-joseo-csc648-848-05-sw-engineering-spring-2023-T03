4e989f4b5b7b63007bd986002d0ee9c513c15916
Duncan Herington <87780351+Dherington1@users.noreply.github.com>
2023-04-20 15:27:54 -0700

    JWT for logged in status implemented, JWT tokens working saving the users login. Navbar changes now when user logs in

--- application/server/controllers/user-routes.js
+++ application/server/controllers/user-routes.js
@@ -3,9 +3,8 @@ const AWS = require('aws-sdk');
 const { v4: uuidv4 } = require('uuid');
 const { client } = require('../db/db');
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
-// user session logging
-const session = require('express-session');
 // password hashing
 const bcrypt = require('bcrypt');
 
@@ -111,6 +110,7 @@ router.post('/register', upload.single('profile_picture'), async (req, res) => {
               password: hashedPassword,
               profile_picture: data.Location,
             });
+
             res
               .status(201)
               .json({ message: 'User created successfully', user: newUser });
@@ -120,9 +120,7 @@ router.post('/register', upload.single('profile_picture'), async (req, res) => {
     };
     const imgURL = await uploadImg(file);
 
-    // Create a session and return a success message
-    req.session.user = newUser;
-    //res.redirect('/');
+    res.redirect('/');
   } catch (err) {
     console.error(err);
     console.log(err.message);
@@ -148,7 +146,6 @@ router.post('/login', async (req, res) => {
     }
 
     // take in password and compare with user password from email
-    console.log(dbUserData.password);
     const passwordMatch = await bcrypt.compare(password, dbUserData.password);
 
     if (!passwordMatch) {
@@ -156,32 +153,24 @@ router.post('/login', async (req, res) => {
       return;
     }
 
-    req.session.save(() => {
-      req.session.user_id = dbUserData.id;
-      req.session.username = dbUserData.username;
-      req.session.loggedIn = true;
-
-      console.log('sessions saved: ');
-      res.json({ user: dbUserData, message: 'You are now logged in!' });
-    });
+    if (dbUserData && passwordMatch) {
+      const token = jwt.sign({
+          userId: dbUserData._id,
+        },
+        process.env.JWT_SECRET, {
+          expiresIn: '24h',
+        }
+      );
+      res.json({
+        success: true,
+        message: 'Authentication successful!',
+        token,
+      });
+    } 
   } catch (err) {
+    console.log(err.message)
     res.status(500).json({ message: 'An error occurred during login.' });
   }
 });
-
-// logout route
-router.post('/logout', (req, res) => {
-  if (req.session.loggedIn) {
-    req.session.destroy(() => {
-      console.log('You are now logout!');
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
-});
-
-//upload profile pic
-router.post('/profilepic', (req, res) => {});
 
 module.exports = router;
