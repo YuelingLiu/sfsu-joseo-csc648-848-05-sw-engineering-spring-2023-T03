d04f6e8333378c42c8906ce73c0b5820e68929db
Nathan <nateyl.b@gmail.com>
2023-04-24 20:29:50 -0700

    setting up to fix s3 v3 issue

--- application/server/controllers/user-routes.js
+++ application/server/controllers/user-routes.js
@@ -1,22 +1,22 @@
-const router = require('express').Router();
-const AWS = require('aws-sdk');
-const { v4: uuidv4 } = require('uuid');
-const { client } = require('../db/db');
-const User = require('../models/User');
-const jwt = require('jsonwebtoken');
+const router = require("express").Router();
+const AWS = require("aws-sdk");
+const { v4: uuidv4 } = require("uuid");
+const { client } = require("../db/db");
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 // password hashing
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 // image storing
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './uploads/');
+    cb(null, "./uploads/");
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
+    cb(null, Date.now() + "-" + file.originalname);
   },
 });
 // Initialize Multer with the storage options
@@ -29,10 +29,10 @@ const s3 = new AWS.S3({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
-router.get('/followers', async (req, res) => {
+router.get("/followers", async (req, res) => {
   const userID = req.query.id;
   if (!userID) {
-    res.status(400).json({ error: 'Missing user ID.' });
+    res.status(400).json({ error: "Missing user ID." });
     return;
   }
 
@@ -45,10 +45,10 @@ router.get('/followers', async (req, res) => {
     });
 });
 
-router.get('/following', async (req, res) => {
+router.get("/following", async (req, res) => {
   const userID = req.query.id;
   if (!userID) {
-    res.status(400).json({ error: 'Missing user ID.' });
+    res.status(400).json({ error: "Missing user ID." });
     return;
   }
 
@@ -62,12 +62,12 @@ router.get('/following', async (req, res) => {
 });
 
 // register route
-router.post('/register', upload.single('profile_picture'), async (req, res) => {
+router.post("/register", upload.single("profile_picture"), async (req, res) => {
   try {
-    console.log('in register route');
+    console.log("in register route");
     // take input from website
     const username = req.body.username;
-    console.log('this is username:' + username);
+    console.log("this is username:" + username);
     const email = req.body.email;
     const password = req.body.password;
 
@@ -76,7 +76,7 @@ router.post('/register', upload.single('profile_picture'), async (req, res) => {
     if (existingUser) {
       return res
         .status(409)
-        .json({ message: 'A user with this email already exists' });
+        .json({ message: "A user with this email already exists" });
     }
 
     // Hash the password
@@ -88,51 +88,52 @@ router.post('/register', upload.single('profile_picture'), async (req, res) => {
     const uploadImg = (file) => {
       // check if buffer is working
       if (!file.buffer) {
-        return Promise.reject(new Error('File buffer is not available'));
+        return Promise.reject(new Error("File buffer is not available"));
       }
       const uploadParams = {
         Bucket: process.env.AWS_BUCKET_NAME,
-        Key: uuidv4() + '-' + file.originalname,
+        Key: uuidv4() + "-" + file.originalname,
         Body: file.buffer,
-        ContentEncoding: 'base64',
+        ContentEncoding: "base64",
         ContentType: file.mimetype,
       };
       return new Promise(async (resolve, reject) => {
         await s3.upload(uploadParams, async function (err, data) {
           if (err) {
-            console.log('Upload error: ', err);
+            console.log("Upload error: ", err);
             reject(err);
-          } else {
-            console.log('Upload Success: ', data.Location);
-            newUser = await User.create({
-              username: username,
-              email: email,
-              password: hashedPassword,
-              profile_picture: data.Location,
-            });
-
-            res
-              .status(201)
-              .json({ message: 'User created successfully', user: newUser });
+          } else{
+            console.log("Upload Success: ", data.Location);
+            resolve(data.Location)
           }
         });
       });
     };
     const imgURL = await uploadImg(file);
+    newUser = await User.create({
+      username: username,
+      email: email,
+      password: hashedPassword,
+      profile_picture: imgURL
+    });
 
-    res.redirect('/');
+    res
+      .status(201)
+      .json({ message: "User created successfully", user: newUser });
+
+    // res.redirect("/");
   } catch (err) {
     console.error(err);
     console.log(err.message);
     res.status(500).json({
-      message: 'An error occurred during register.',
+      message: "An error occurred during register.",
       error: err.message,
     });
   }
 });
 
 // login route
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     // take inputs
     const email = req.body.email;
@@ -141,7 +142,7 @@ router.post('/login', async (req, res) => {
     // check if email is in db
     const dbUserData = await User.getByEmail(email);
     if (!dbUserData) {
-      res.status(400).json({ message: 'No user with that email address!' });
+      res.status(400).json({ message: "No user with that email address!" });
       return;
     }
 
@@ -149,27 +150,29 @@ router.post('/login', async (req, res) => {
     const passwordMatch = await bcrypt.compare(password, dbUserData.password);
 
     if (!passwordMatch) {
-      res.status(400).json({ message: 'Incorrect password!' });
+      res.status(400).json({ message: "Incorrect password!" });
       return;
     }
 
     if (dbUserData && passwordMatch) {
-      const token = jwt.sign({
+      const token = jwt.sign(
+        {
           userId: dbUserData._id,
         },
-        process.env.JWT_SECRET, {
-          expiresIn: '24h',
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "24h",
         }
       );
       res.json({
         success: true,
-        message: 'Authentication successful!',
+        message: "Authentication successful!",
         token,
       });
-    } 
+    }
   } catch (err) {
-    console.log(err.message)
-    res.status(500).json({ message: 'An error occurred during login.' });
+    console.log(err.message);
+    res.status(500).json({ message: "An error occurred during login." });
   }
 });
 
