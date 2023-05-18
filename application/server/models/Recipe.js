ad071c11dd5e17fee05c44d8a43fb2043c91a15a
Nathan <nateyl.b@gmail.com>
2023-05-17 19:39:22 -0700

    pasredINt

--- application/server/models/Recipe.js
+++ application/server/models/Recipe.js
@@ -6,6 +6,8 @@ const { client } = require("../db/db");
 class Recipe {
   static async create(recipeParam, ingredientsParam, instructionsParam) {
     console.log("recipeParam: ",recipeParam);
+    console.log("ingredients: ",ingredientsParam);
+    console.log("instructionsParam: ",instructionsParam);
     const recipe = (await knex('recipes').insert(recipeParam).returning('*'))[0];
     const ingredients = [];
     const instructions = [];
